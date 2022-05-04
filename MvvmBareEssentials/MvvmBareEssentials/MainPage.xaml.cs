bc7c28a7c53b3ddcd5fd67448461f15c0ce4bfb3
maximlesy <maximlesy@gmail.com>
2022-05-04 17:52:10 +0200

    Intial commit

---
+++ MvvmBareEssentials/MvvmBareEssentials/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MvvmBareEssentials.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MvvmBareEssentials
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ MvvmBareEssentials/MvvmBareEssentials/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MvvmBareEssentials.ViewModels
+{
+    //Viewmodels has to implement INotifyPropertyChanged
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set 
+            { 
+                name = value;
+                RaisePropertyChanged(nameof(Name));
+            }
+        }
+
+        public ICommand SetDefaultName =>
+            new Command<string>(name => Name = name);
+
+        private void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+    }
+}
